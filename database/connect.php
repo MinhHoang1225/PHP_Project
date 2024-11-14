3d17e1f7eb9838dc25a3419b7c27c4e1a1ce93c6
MinhHoang1225 <minhhoang121225@gmail.com>
2024-11-14 11:02:07 +0700

    connect database

---
+++ database/connect.php
@@ -0,0 +1,18 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "sneaker_home";
+
+// Tạo kết nối
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Kiểm tra kết nối
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+// echo "Connected successfully";
+
+// Đóng kết nối
+// $conn->close();
+?>
