4009797bbbac87a725af47a37306b9a1b0e5c503
ChăngSeven2k5 <changcoor7@gmail.com>
2024-11-17 18:12:43 +0700

    code about us

---
+++ component/about.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+    <link rel="stylesheet" href="../assets/css/about.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+</head>
+<body>
+    <!--------------------------------- About---------------------------------------------------- -->
+<div class="about">
+        <div class="about-us">
+            <h2 class="heading-title">About Us</h2>
+            <p class="heading-description">Chào mừng bạn đến với <b>Sneakers Web</b>, nơi mang đến những đôi giày thể thao và phụ kiện thời trang đỉnh cao dành cho mọi phong cách. 
+                Chúng tôi không chỉ là một cửa hàng trực tuyến, mà còn là nơi kết nối đam mê thời trang và phong cách sống hiện đại.</p>
+        </div>
+        <div class="who-we-are">
+            <div class="row">
+                <span class="row-line"></span>
+            </div>
+            <div class="heading-who-we-are">
+                <h2 class="heading-title">Who We Are?</h2>
+                <p class="heading-description"><b>Sneakers Web</b> là thương hiệu dành cho những ai yêu thích sự kết hợp giữa thời trang và sự tiện dụng. Với đội ngũ đam mê và chuyên nghiệp, 
+                    chúng tôi cam kết mang đến cho bạn những sản phẩm chất lượng nhất, phù hợp với mọi lứa tuổi và xu hướng.</p>
+            </div>
+            <div class="image">
+                <img src="../assets/img/who_we_are.jpg" alt="" class="img-who-we-are">
+            </div>
+        </div>
+        <div class="what-we-do">
+            <div class="row">
+                <span class="row-line"></span>
+            </div>
+            <div class="heading-what-we-do">
+                <h2 class="heading-title">What We Do?</h2>
+                <p class="heading-description">
+                    <ul class="menu-description">
+                        <li class="menu-item"><b>Cung cấp giày thể thao đa dạng:</b> Từ phong cách cổ điển đến hiện đại, chúng tôi luôn cập nhật những mẫu giày phù hợp với xu hướng thời trang toàn cầu.</li>
+                        <li class="menu-item"><b>Phụ kiện đi kèm:</b> Áo và tất thời trang giúp bạn hoàn thiện phong cách cá nhân.</li>
+                        <li class="menu-item"><b>Hỗ trợ mua sắm tiện lợi:</b> Với giao diện thân thiện, bạn dễ dàng tìm kiếm, chọn mua và thanh toán trực tuyến chỉ với vài bước đơn giản.</li>
+                        <li class="menu-item"><b>Dịch vụ khách hàng chuyên nghiệp:</b> Chúng tôi luôn sẵn sàng hỗ trợ và tư vấn để mang đến trải nghiệm mua sắm tốt nhất.</li>
+                    </ul>
+                </p>
+            </div>
+        </div>
+        <div class="our-team">
+            <div class="row">
+                <span class="row-line"></span>
+            </div>
+            <h2 class="heading-title">Our Team</h2>
+            <div class="card">
+            <div class="card-member">
+                    <div class="profile-image">
+                    <img src="../assets/img/who_we_are.jpg" alt="Amanda Care">
+                    </div>
+                    <div class="social-links">
+                    <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                    <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                    <a href="#"><i class="fa-solid fa-envelope"></i></i></a>
+                    </div>
+                    <div class="info-member">
+                    <h2 class="name-member">Đoàn Minh Hoàng</h2>
+                    <p class="position-member">Leader</p>
+                    </div>
+                </div>
+                <div class="card-member">
+                    <div class="profile-image">
+                    <img src="../assets/img/who_we_are.jpg" alt="Amanda Care">
+                    </div>
+                    <div class="social-links">
+                    <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                    <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                    <a href="#"><i class="fa-solid fa-envelope"></i></i></a>
+                    </div>
+                    <div class="info-member">
+                    <h2 class="name-member">Trần Văn Vinh</h2>
+                    <p class="position-member">Member</p>
+                    </div>
+                </div>
+                <div class="card-member">
+                    <div class="profile-image">
+                    <img src="../assets/img/who_we_are.jpg" alt="Amanda Care">
+                    </div>
+                    <div class="social-links">
+                    <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                    <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                    <a href="#"><i class="fa-solid fa-envelope"></i></i></a>
+                    </div>
+                    <div class="info-member">
+                    <h2 class="name-member">Hồ Thị Tiếp</h2>
+                    <p class="position-member">Member</p>
+                    </div>
+                </div>
+                <div class="card-member">
+                    <div class="profile-image">
+                    <img src="../assets/img/who_we_are.jpg" alt="Amanda Care">
+                    </div>
+                    <div class="social-links">
+                    <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                    <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                    <a href="#"><i class="fa-solid fa-envelope"></i></i></a>
+                    </div>
+                    <div class="info-member">
+                    <h2 class="name-member">Bờ Nướch Hoài Tiên</h2>
+                    <p class="position-member">Member</p>
+                    </div>
+                </div>
+                <div class="card-member">
+                    <div class="profile-image">
+                    <img src="../assets/img/who_we_are.jpg" alt="Amanda Care">
+                    </div>
+                    <div class="social-links">
+                    <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                    <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                    <a href="#"><i class="fa-solid fa-envelope"></i></i></a>
+                    </div>
+                    <div class="info-member">
+                    <h2 class="name-member">Coor Chăng</h2>
+                    <p class="position-member">Member</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- ===================================================================== -->
+</body>
+</html>
