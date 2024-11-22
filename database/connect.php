700e0d085087aae0733a55a357df12616d47683f
MinhHoang1225 <minhhoang121225@gmail.com>
2024-11-22 21:07:48 +0700

    connect new database

--- database/connect.php
+++ database/connect.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "sneaker_home";
+$dbname = "php_project";
 
 // Tạo kết nối
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -11,7 +11,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-// echo "Connected successfully";
+echo "Connected successfully";
 
 // Đóng kết nối
 // $conn->close();
