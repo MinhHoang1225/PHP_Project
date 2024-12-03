abc1e95905d0e255e8a684fb8afec21a9dc9cbbc
hothitiep02 <htt301202@gmail.com>
2024-12-03 09:28:15 +0700

    Fix addtofavourite

--- view/show_product.php
+++ view/show_product.php
@@ -255,11 +255,12 @@ $result = $stmt->get_result();
                                 }
                             ?>
                         </a>
-                        <a href="#" class="container d-flex justify-content-center align-items-center">
-                            <div class="heart-icon">
+                        <div class="container d-flex justify-content-center align-items-center">
+                            <div class="heart-icon" onclick="addToFavourites(<?php echo $row['product_id']; ?>)">
                                 <i class="bi bi-heart-fill"></i>
                             </div>
-                        </a>
+                        </div>
+                        
                         <a href="#" class="container d-flex justify-content-center align-items-center">
                             <div class="cart-icon">
                                 <i class="bi bi-bag-plus-fill"></i>
@@ -285,6 +286,22 @@ $result = $stmt->get_result();
     ?>
 </div>
 </div>
+
+<script>
+            function addToFavourites(productId) {
+                const xhr = new XMLHttpRequest();
+                xhr.open("POST", "../component/add_to_favourite.php", true);
+                xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+                xhr.onreadystatechange = function () {
+                    if (xhr.readyState === 4 && xhr.status === 200) {
+                        alert(xhr.responseText); // Hiển thị thông báo từ server
+                    }
+                };
+
+                xhr.send("product_id=" + productId);
+            }
+        </script>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
